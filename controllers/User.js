de8b99bf731e85d4bb2301cf18086f5ed563bb5a
Ali <4189737+alidawud@users.noreply.github.com>
2020-07-31 00:28:00 +0800

    add update me

--- controllers/User.js
+++ controllers/User.js
@@ -116,10 +116,120 @@ module.exports = {
   async me(ctx) {
     const user = ctx.state.user;
 
-    if (!user) {
+    if (!user || !user.id) {
       return ctx.badRequest(null, [{ messages: [{ id: 'No authorization header was found' }] }]);
     }
 
-    ctx.body = sanitizeUser(user);
+    const newUser = await strapi.query('user', 'users-permissions').findOne({
+      id: user.id
+    });
+
+    ctx.body = sanitizeUser(newUser);
   },
+
+  /**
+   * Update a/an user record.
+   * @return {Object}
+   */
+
+  async updateMe(ctx) {
+    const advancedConfigs = await strapi
+      .store({
+        environment: '',
+        type: 'plugin',
+        name: 'users-permissions',
+        key: 'advanced',
+      })
+      .get();
+
+    // Yoo.cash start
+    // const { id } = ctx.params;
+    if (!ctx.state.user || !ctx.state.user.id) {
+      return ctx.badRequest(null, [{ messages: [{ id: 'No authorization header was found' }] }]);
+    }
+    const id = ctx.state.user.id;
+    // Yoo.cash end
+
+    const { email, username, password } = ctx.request.body;
+
+    const user = await strapi.plugins['users-permissions'].services.user.fetch({
+      id,
+    });
+
+    if (_.has(ctx.request.body, 'email') && !email) {
+      return ctx.badRequest('email.notNull');
+    }
+
+    if (_.has(ctx.request.body, 'username') && !username) {
+      return ctx.badRequest('username.notNull');
+    }
+
+    if (_.has(ctx.request.body, 'password') && !password && user.provider === 'local') {
+      return ctx.badRequest('password.notNull');
+    }
+
+    if (_.has(ctx.request.body, 'username')) {
+      const userWithSameUsername = await strapi
+        .query('user', 'users-permissions')
+        .findOne({ username });
+
+      if (userWithSameUsername && userWithSameUsername.id != id) {
+        return ctx.badRequest(
+          null,
+          formatError({
+            id: 'Auth.form.error.username.taken',
+            message: 'username.alreadyTaken.',
+            field: ['username'],
+          })
+        );
+      }
+    }
+
+    if (_.has(ctx.request.body, 'email') && advancedConfigs.unique_email) {
+      const userWithSameEmail = await strapi.query('user', 'users-permissions').findOne({ email });
+
+      if (userWithSameEmail && userWithSameEmail.id != id) {
+        return ctx.badRequest(
+          null,
+          formatError({
+            id: 'Auth.form.error.email.taken',
+            message: 'Email already taken',
+            field: ['email'],
+          })
+        );
+      }
+    }
+
+    let updateData = {
+      ...ctx.request.body,
+    };
+
+    if (_.has(ctx.request.body, 'password') && password === user.password) {
+      delete updateData.password;
+    }
+
+    // Yoo.cash start: Don't update sensitive data
+    delete updateData['id'];
+    delete updateData['username'];
+    delete updateData['provider'];
+    delete updateData['password'];
+    delete updateData['resetPasswordToken'];
+    delete updateData['confirmed'];
+    delete updateData['blocked'];
+    delete updateData['role'];
+    delete updateData['providerAvatar'];
+    delete updateData['providerUID'];
+    delete updateData['created_by'];
+    delete updateData['updated_by'];
+    delete updateData['created_at'];
+    delete updateData['updated_at'];
+    // Yoo.cash end
+
+
+    const data = await strapi.plugins['users-permissions'].services.user.edit({ id }, updateData);
+
+    ctx.send(sanitizeUser(data));
+  },
+
 };
+
